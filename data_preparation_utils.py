b1afc166c29ef4719026e9da75c149a39c446ef3
Baris Bakirdoven <bbakirdoven@sabanciuniv.edu>
2023-07-25 14:17:04 +0200

    fixed import errors

--- data_preparation_utils.py
+++ data_preparation_utils.py
@@ -7,51 +7,6 @@ import os
 from PIL import Image
 import matplotlib.pyplot as plt
 
-#Get the directories of burst and non-burst images
-def get_imgs_directory():
-    c_dir = os.getcwd()
-    relative_burst_dir = os.path.join('data', 'burst')
-    relative_nburst_dir = os.path.join('data', 'no_burst')
-
-    burst_image_dir = os.path.join(c_dir, relative_burst_dir)
-    nburst_image_dir = os.path.join(c_dir, relative_nburst_dir)
-    
-
-    print(burst_image_dir)
-    print(nburst_image_dir)
-    
-    return burst_image_dir, nburst_image_dir
-
-#resize and fill the images to be the target size: likely 256x256 for possible use in CNN's
-def resize_and_fill(image_path, target_size):
-    img = Image.open(image_path)
-    img.thumbnail(target_size)
-
-    new_img = Image.new("L", target_size)
-    new_img.paste(img, ((target_size[0] - img.size[0]) // 2,
-                        (target_size[1] - img.size[1]) // 2))
-
-    return new_img
-
-#Use the resize and fill function for each image in the image directory and save it on top of these images: Note that the old versions of images are deleted
-def resize_and_save_img_in_directory(img_directory, target_size):
-    for file_name in os.listdir(img_directory):
-    #print(file_name)
-        if file_name.endswith('.png'):
-            image_path = os.path.join(img_directory, file_name)
-            new_img = resize_and_fill(image_path, target_size)
-            new_img.save(os.path.join(img_directory, file_name)) #save new image
-            
-        
-    plt.imshow(new_img)
-    plt.axis("off")
-    plt.show()
-    
-#Rescale the images
-def rescale(image, label):
-    return image/255. , label
-
-
 #Obtain training and validation datasets from image directories      
 def get_datasets():
     
@@ -92,10 +47,59 @@ def get_datasets():
         crop_to_aspect_ratio=False,
     )
     
+    print("get datasets works")
     return train_ds, validation_ds
-#Returns the number of batches
-def get_img_num(ds):
-    #Determine the number of images in train and validation datasets
-    ds_total_size = tf.data.experimental.cardinality(ds).numpy()
 
-    return ds_total_size
+
+if __name__ == "__main__":
+    #Get the directories of burst and non-burst images
+    def get_imgs_directory():
+        c_dir = os.getcwd()
+        relative_burst_dir = os.path.join('data', 'burst')
+        relative_nburst_dir = os.path.join('data', 'no_burst')
+
+        burst_image_dir = os.path.join(c_dir, relative_burst_dir)
+        nburst_image_dir = os.path.join(c_dir, relative_nburst_dir)
+        
+
+        print(burst_image_dir)
+        print(nburst_image_dir)
+        print("get imgs direcotry works")
+        
+        return burst_image_dir, nburst_image_dir
+
+    #resize and fill the images to be the target size: likely 256x256 for possible use in CNN's
+    def resize_and_fill(image_path, target_size):
+        img = Image.open(image_path)
+        img.thumbnail(target_size)
+
+        new_img = Image.new("L", target_size)
+        new_img.paste(img, ((target_size[0] - img.size[0]) // 2,
+                            (target_size[1] - img.size[1]) // 2))
+        print("resize and fill works")
+
+        return new_img
+
+    #Use the resize and fill function for each image in the image directory and save it on top of these images: Note that the old versions of images are deleted
+    def resize_and_save_img_in_directory(img_directory, target_size):
+        for file_name in os.listdir(img_directory):
+        #print(file_name)
+            if file_name.endswith('.png'):
+                image_path = os.path.join(img_directory, file_name)
+                new_img = resize_and_fill(image_path, target_size)
+                new_img.save(os.path.join(img_directory, file_name)) #save new image
+                
+            
+        plt.imshow(new_img)
+        plt.axis("off")
+        plt.show()
+        print("image showed, resize and save img in directory works")    
+
+ 
+    #Returns the number of batches
+    def get_img_num(ds):
+        #Determine the number of images in train and validation datasets
+        ds_total_size = tf.data.experimental.cardinality(ds).numpy()
+
+        print("get img num works")
+        return ds_total_size
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from model_builder_class import Model_builder
-from data_preparation import get_imgs_directory, resize_and_save_img_in_directory, get_datasets, get_img_num
-from train_utils import create_config, init_wandb_from_config, build_and_train
+from data_preparation_utils import get_datasets
+from train_utils import build_and_train, create_config, init_wandb_from_config
 
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, GlobalAveragePooling2D
@@ -23,18 +22,18 @@ def main():
     #Fix the random generator seeds for better reproducibility
     tf.random.set_seed(67)
     np.random.seed(67)
-    
+    print("checkpoint 1")
     #Obtain the path of the YAML file and get a config file
-    c_dir = os.getcwd
+    c_dir = os.getcwd()
     config_path = os.path.join(c_dir, 'config.yaml')
     config = create_config(config_path)
-    
+    print("checkpoint 2")
     #Obtain the datasets, using get_datasets function from data_preparation
     train_ds, validation_ds = get_datasets()
-
+    print("checkpoint 3")
     #Use config file to obtain sweep_id
     sweep_id = wandb.sweep(config, entity=config['entity'], project=config['project'])
-
+    print("checkpoint 4")
     #Define a training function that uses the config file to initialize wandb and build/train the model using the datasets
     def train_fn():
         run = init_wandb_from_config(config)
@@ -42,6 +41,6 @@ def main():
 
     #Calling the defined function for wandb to do hyperparameter tuning
     wandb.agent(sweep_id, train_fn)
-
+    print("checkpoint 5")
 if __name__ == "__main__":
     main()
