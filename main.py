a1585ebc6cf36d8f1822d4e17b70b64b206df1a4
Baris Bakirdoven <bbakirdoven@sabanciuniv.edu>
2023-07-31 10:09:58 +0200

    Improved sweep script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+from itertools import islice
 import os
 
 import numpy as np
@@ -25,11 +26,11 @@ def main(config_name):
         entity="i4ds_radio_sunburst_detection",
     )
     del config
-
-    #read the main excel file into df
-    main_df = pd.read_excel('main_list.xlsx')
+    
+    configured_burst_df = pd.read_excel('configured_burst.xlsx')
+    configured_noburst_df = pd.read_excel('configured_noburst.xlsx')
         
-    train_ds, validation_ds, test_ds = get_datasets(main_df)
+    train_ds, validation_ds, test_ds, train_df, val_df, test_df = get_datasets(configured_burst_df, configured_noburst_df)
     # Log number of images in training and validation datasets
     # TODO: Log number of images in test dataset
 
@@ -57,15 +58,21 @@ def main(config_name):
     )
     artifact.add_file(os.path.join(wandb.run.dir, "model.keras"))
 
+    print("befor evaluate")
     # Evaluate model
     eval = model.evaluate(test_ds)
     eval_metrics = dict(zip(model.metrics_names, eval))  # Python magic
     wandb.log(eval_metrics)
-
+    
+    print("before predict")
     # Calculate other things
     y_pred_proba = model.predict(test_ds).flatten()
+    print("predict-1")
     y_pred = np.where(y_pred_proba > 0.5, 1, 0)
-    y_true = np.concatenate([y for x, y in test_ds], axis=0).flatten()
+    print("predict-2")
+    steps = len(test_ds)  # This will give the number of batches in the test_ds
+    y_true = np.concatenate([y for x, y in islice(test_ds, steps)], axis=0).flatten()   
+    print("after predict")
 
     # Plot ROC curve
     fig = plot_roc_curve(y_true, y_pred_proba)
@@ -75,13 +82,13 @@ def main(config_name):
     wandb.log(
         {
             "Confusion Matrix": wandb.plot.confusion_matrix(
-                y_true=y_true, preds=y_pred, class_names=train_ds.class_names
+                y_true=y_true, preds=y_pred, class_names=list(train_ds.class_indices.keys())
             )
         }
     )
 
     # Upload classification report to wandb
-    log_wandb_print_class_report(y_true, y_pred, target_names=train_ds.class_names)
+    log_wandb_print_class_report(y_true, y_pred, target_names=list(train_ds.class_indices.keys()))
 
 
 if __name__ == "__main__":
