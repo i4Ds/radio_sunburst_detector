487ce6c5e6967991558d1aab769bcc4d0a67a29b
Baris Bakirdoven <bbakirdoven@sabanciuniv.edu>
2023-07-31 15:15:09 +0200

    Seperated Dataframe configuration

--- train_utils.py
+++ train_utils.py
@@ -45,37 +45,5 @@ def init_wandb_from_config(config):
     return run
 
 
-def build_and_train(run, train_ds, validation_ds):
-    # Create a model_builder object, build and compile the model inside it and retrieve it as "model"
-    model_builder_object = ModelBuilder.Model_builder(
-        input_shape=(256, 256, 1),
-        num_classes=2,
-        encoder_filters=run.config.encoder_filters,
-        encoder_kernel_size=run.config.encoder_kernel_size,
-        encoder_l1=run.config.encoder_l1,
-        units=run.config.units,
-        dropout=run.config.dropout,
-        weight_initialization=run.config.weight_initialization,
-        batch_size=run.config.batch_size,
-        learning_rate=run.config.learning_rate,
-        before_encoder_loop=run.config.before_encoder_loop,
-        num_dense_layers=run.config.num_dense_layers,
-        neurons_dense_layer=run.config.neurons_dense_layer,
-        activation=run.config.activation,
-    )
-    model_builder_object.build()
-    model = model_builder_object.compile()
-
-    history = model.fit(
-        train_ds,
-        validation_data=validation_ds,
-        epochs=run.config.epochs,
-        batch_size=run.config.batch_size,
-        callbacks=[WandbCallback()],  # WandbCallback logs metrics and final model
-    )
-
-    return model, history
-
-
 if __name__ == "__main__":
     print("hello")
