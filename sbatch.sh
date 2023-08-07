ea9a9dd0615522297a8f23d259e856816093f575
Vincenzo Timmel <vincenzo.timmel@fhnw.ch>
2023-08-07 15:26:26 +0200

    Increased epochs

--- sbatch.sh
+++ sbatch.sh
@@ -2,4 +2,5 @@
 #SBATCH --time=04:00:00
 #SBATCH --cpus-per-task=1  # 
 #SBATCH --gres=gpu:1
-wandb agent i4ds_radio_sunburst_detection/radio_sunburst_detection/rlavlwvy
+#SBATCH --partition=top6
+wandb agent i4ds_radio_sunburst_detection/radio_sunburst_detection/9wf1m1xo
