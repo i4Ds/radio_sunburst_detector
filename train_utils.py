e964e87f054255649bd7d7d0d3a852551895ff08
Vincenzo Timmel <vincenzo.timmel@fhnw.ch>
2023-08-10 18:47:34 +0200

    Fixed main script.

--- train_utils.py
+++ train_utils.py
@@ -8,6 +8,14 @@ def load_config(config_path):
         config = yaml.safe_load(config_file)
     return config
 
+def train_model(model, train_ds, validation_ds, epochs, callbacks):
+    model.fit(
+        train_ds,
+        validation_data=validation_ds,
+        epochs=epochs,
+        callbacks=callbacks,
+    )
+    return model
 
 # Initialize wandb using the config
 def init_wandb_from_config(config):
@@ -18,6 +26,47 @@ def init_wandb_from_config(config):
         config=config["parameters"],
     )
     return run
+
+def check_artifact_exists(project_name, entity_name, artifact_name):
+    # Construct artifact URL.
+    artifact_url = f"{entity_name}/{project_name}/{artifact_name}"
+    try:
+        # Try fetching the artifact.
+        artifact = wandb.Api().artifact(artifact_url)
+        return True
+    except wandb.CommError:
+        # Artifact does not exist.
+        return False
+
+def get_or_train_model(config_name, project_name, entity_name):
+    artifact_name = f"{config_name}:latest"
+    
+    # Check if artifact exists.
+    if check_artifact_exists(project_name, entity_name, artifact_name):
+        # Download the artifact.
+        artifact = wandb.use_artifact(artifact_name)
+        # Load the Keras model from artifact.
+        model_path = artifact.get_path("model.keras").download()
+        model = load_model(model_path)
+    else:
+        # Train the model.
+        model = train_model()
+        
+        # Save the model locally.
+        model_path = os.path.join(wandb.run.dir, "model.keras")
+        model.save(model_path)
+        
+        # Create and upload artifact to wandb.
+        artifact = wandb.Artifact(
+            config_name,
+            type="model",
+            description="trained model",
+            metadata=dict(config_name=config_name)
+        )
+        artifact.add_file(model_path)
+        wandb.log_artifact(artifact)
+    
+    return model
 
 
 if __name__ == "__main__":
