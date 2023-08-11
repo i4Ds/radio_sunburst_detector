f69b7d56b891c354415717e5d52b1b59db4e5c67
Vincenzo Timmel <vincenzo.timmel@fhnw.ch>
2023-08-11 13:49:17 +0200

    Improved data preparation

--- data_preparation_utils.py
+++ data_preparation_utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 from configure_dataframes import directory_to_dataframe
-
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # Obtain training, and test datasets from dataframes
 def get_datasets(
@@ -39,9 +39,6 @@ def get_datasets(
     test_df = data_df.iloc[-test_len:]
     train_df = data_df.iloc[:train_len]
 
-    # Assert that the dataframes are correct
-    assert np.intersect1d(train_df["file_path"], test_df["file_path"]).size == 0
-
     # Create class balance in the dataframes
     if burst_frac:
         train_df = update_class_balance_per_instrument(train_df, burst_frac)
@@ -58,6 +55,11 @@ def get_datasets(
     # Print out class balance
     print_class_balance(train_df, "train")
     print_class_balance(test_df, "test")
+    
+    # Final asserts
+    assert train_df.start_time.max() < test_df.start_time.min()
+    # Assert that the dataframes are correct
+    assert np.intersect1d(train_df["file_path"], test_df["file_path"]).size == 0
 
     return train_df, test_df
 
@@ -103,17 +105,60 @@ def print_class_balance(df, dataset_name):
 
 if __name__ == "__main__":
     data_df = directory_to_dataframe()
-    train_ds, test_ds, train_df, test_df = get_datasets(data_df, return_dfs=True)
+    # Create datasets
+    train_df, test_df  = get_datasets(data_df, train_size=0.7, test_size=0.3, burst_frac=0.5, sort_by_time=True, only_unique_time_periods=True)
+                                            
+    # Update datasets
+    val_df, test_df = test_df.iloc[:len(test_df)//2], test_df.iloc[len(test_df)//2:]
 
     train_df.to_excel("train.xlsx")
     test_df.to_excel("test.xlsx")
     data_df.to_excel("data.xlsx")
 
+    # Data gen
+    datagen = ImageDataGenerator()
+
+    # Create datasets
+    train_ds = datagen.flow_from_dataframe(
+        train_df,
+        x_col="file_path",
+        y_col="label",
+        batch_size=64,
+        seed=42,
+        shuffle=True,
+        class_mode="binary",
+        target_size=(256, 256),
+        color_mode="rgb",
+    )
+    val_ds = datagen.flow_from_dataframe(
+        val_df,
+        x_col="file_path",
+        y_col="label",
+        batch_size=64,
+        seed=42,
+        shuffle=False,
+        class_mode="binary",
+        target_size=(256, 256),
+        color_mode="rgb",
+    )
+
+    test_ds = datagen.flow_from_dataframe(
+        test_df,
+        x_col="file_path",
+        y_col="label",
+        batch_size=64,
+        seed=42,
+        shuffle=False,
+        class_mode="binary",
+        target_size=(256, 256),
+        color_mode="rgb",
+    )
+
     class_names = list(train_ds.class_indices.keys())
     for ds, ds_name, df in zip(
-        [train_ds, test_ds],
-        ["train", "test"],
-        [train_df, test_df],
+        [train_ds, val_ds, test_ds],
+        ["train", "val", "test"],
+        [train_df, val_df, test_df],
     ):
         # Plot some images
         images, labels = next(
